a9b1df3fdbf747df61698752c4a8f4ab9e5cf378
Omar Samara <omsamara@ncsu.edu>
2019-10-18 21:15:33 -0400

    Refactored to include banana. Preliminary messing around with the ConstraintLayout

--- app/src/main/java/com/bman/bananaslicerremastered/ui/MainActivity.kt
+++ app/src/main/java/com/bman/bananaslicerremastered/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.bman.bananaslicerremastered.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bman.bananaslicerremastered.R
@@ -27,19 +26,15 @@ class MainActivity : AppCompatActivity() {
         // https://stackoverflow.com/questions/53903762/viewmodelproviders-is-deprecated-in-1-1-0
         val viewModel = ViewModelProvider(this, factory).get(QuotesViewModel::class.java)
 
-        viewModel.getQuotes().observe(this, Observer { quotes ->
-            val stringBuilder = StringBuilder()
-            quotes.forEach {quote ->
-                stringBuilder.append("$quote\n\n")
-            }
-            textView_quotes.text = stringBuilder.toString()
-        })
+//        viewModel.getQuotes().observe(this, Observer { quotes ->
+//            val stringBuilder = StringBuilder()
+//            quotes.forEach {quote ->
+//                stringBuilder.append("$quote\n\n")
+//            }
+//            textView_quotes.text = stringBuilder.toString()
+//        })
 
         button_add_quote.setOnClickListener {
-            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
-            viewModel.addQuote(quote)
-            editText_quote.setText("")
-            editText_author.setText("")
         }
 
     }
